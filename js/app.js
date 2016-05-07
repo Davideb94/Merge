632846c04731843052d36c87b345074bcb10eafc
Davide Brunetti <davide.brunetti@novetica.org>
2016-05-08 00:13:15 +0100

    Added style to headbar.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
-document.addEventListener("scroll", function(){
-		var header = document.getElementById('header');
-		var section_2 = document.getElementById('section_2');
-		var offset = section_2.offsetTop - 60;
-		var y = document.body.scrollTop;
-		console.log(y);
+window.onload = function(){
+	var login = document.getElementById('login_container');
 	
-		if(y>=offset){
-			header.className = 'colored_header';
+	login.addEventListener("click", function(){
+			alert('Log In!');
+		} 						 
+	);
+	
+	document.addEventListener("scroll", function(){
+			var header = document.getElementById('header');
+		
+			var section_2 = document.getElementById('section_2');
+			var offset = section_2.offsetTop - 60;
+			var y = document.body.scrollTop;
+			console.log(y);
+
+			if(y>=offset){
+				header.className = 'colored_header';
+				login.className = 'colored_login';
+			}
+			if(y<offset){
+				header.className = '';
+				login.className = '';
+			}
+			console.log('MY OFFSET IS: ' + offset);
 		}
-		if(y<offset){
-			header.className = '';
-		}
-		console.log('MY OFFSET IS: ' + offset);
-	}
-);
+	);
+}
