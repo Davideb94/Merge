b6b77dbba8c140969f5c50c292f96d1972a237f2
GabMartino <gabriele-martino@libero.it>
2016-05-22 13:19:50 +0200

    Modified DB structure.

--- merge.sql
+++ merge.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2016 at 06:29 PM
+-- Generation Time: May 22, 2016 at 01:17 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.20
 
@@ -31,13 +31,6 @@ CREATE TABLE `contacts` (
   `IDcontact` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `contacts`
---
-
-INSERT INTO `contacts` (`IDuser`, `IDcontact`) VALUES
-(1, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -49,13 +42,6 @@ CREATE TABLE `desks` (
   `IDdesks` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `desks`
---
-
-INSERT INTO `desks` (`IDuser`, `IDdesks`) VALUES
-(1, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -64,9 +50,12 @@ INSERT INTO `desks` (`IDuser`, `IDdesks`) VALUES
 
 CREATE TABLE `file` (
   `ID` int(3) NOT NULL,
+  `Size` int(40) NOT NULL,
+  `Type` varchar(15) NOT NULL,
   `Name` varchar(50) NOT NULL,
   `Content` longblob NOT NULL,
-  `IDuser` int(3) NOT NULL
+  `IDuser` int(3) NOT NULL,
+  `last_modify` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -88,12 +77,13 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`ID`, `Name`, `Password`, `Email`, `Registration_date`) VALUES
-(1, 'cesare', 'roma', 'cesare@augusto.it', '2016-05-13 19:50:58'),
-(2, ' davide', 'mypass', 'davide@libero.it', '2016-05-13 19:50:58'),
-(3, 'Gabriele', 'martino', 'gafire@hotmail.it', '2016-05-13 20:45:59'),
-(4, 'paola', '.md5(martino)', 'paola@ok.it', '2016-05-13 21:01:42'),
-(5, 'davide', 'c207dcdb5e554ba3a043077fbe6f8dc4', 'davidedb@unito.it', '2016-05-13 21:08:52'),
-(7, 'alessandro', '6e6bc4e49dd477ebc98ef4046c067b5f', 'ale.pronat@minchia.it', '2016-05-14 16:14:49');
+(21, 'gabriele', '444bcb3a3fcf8389296c49467f27e1d6', 'gafire@hotmail.it', '2016-05-14 19:54:51'),
+(23, 'ok', '444bcb3a3fcf8389296c49467f27e1d6', 'gabriele-martino@libero.it', '2016-05-14 19:57:50'),
+(24, 'davide', '6e6bc4e49dd477ebc98ef4046c067b5f', 'davidedb@unito.it', '2016-05-15 20:12:19'),
+(25, 'salvatore', '0d149b90e7394297301c90191ae775f0', 'ok@salvo.it', '2016-05-15 20:33:46'),
+(26, 'vichy', '4d186321c1a7f0f354b297e8914ab240', 'vichy@libero.it', '2016-05-15 22:41:06'),
+(27, 'luca', '444bcb3a3fcf8389296c49467f27e1d6', 'luca@ok.it', '2016-05-16 08:57:43'),
+(28, 'gabriele', '6e6bc4e49dd477ebc98ef4046c067b5f', 'ok@mmm.it', '2016-05-21 14:00:45');
 
 --
 -- Indexes for dumped tables
@@ -137,7 +127,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `IDuser` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `IDuser` int(3) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `file`
 --
@@ -147,7 +137,7 @@ ALTER TABLE `file`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
 -- Constraints for dumped tables
 --
